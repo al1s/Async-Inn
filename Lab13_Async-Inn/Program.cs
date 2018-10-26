9a48a3e2dace2b4f1376846de7e1b84a55e16cf8
alstof <alstof@gmail.com>
2018-10-26 05:50:59 -0700

    Initial master

--- Lab13_Async-Inn/Program.cs
+++ Lab13_Async-Inn/Program.cs
@@ -17,6 +17,7 @@ namespace Lab13_Async_Inn
             CreateWebHostBuilder(args).Build().Run();
         }
 
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
