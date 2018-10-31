8099f22ebc1c69e3fa8b5350f98cbd4dc017bcff
alstof <alstof@gmail.com>
2018-10-31 12:30:07 -0700

    Add ability to be published to Azure

--- Lab13_Async-Inn/Startup.cs
+++ Lab13_Async-Inn/Startup.cs
@@ -9,41 +9,31 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Lab13_AsyncInn.Models;
+using Lab13_AsyncInn.Models.Interfaces;
+using Lab13_AsyncInn.Models.Services;
 
 namespace Lab13_AsyncInn
 {
     public class Startup
     {
         public IConfiguration Configuration { get; }
-        private string _azureConnection = null;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
-        }
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json",
-                             optional: false,
-                             reloadOnChange: true)
-                .AddEnvironmentVariables();
-
-            if (env.IsDevelopment())
-            {
-                builder.AddUserSecrets<Startup>();
-            }
-
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
             Configuration = builder.Build();
         }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<AsyncInnDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            _azureConnection = Configuration["AzureConnection"];
+            //services.AddDbContext<AsyncInnDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddDbContext<AsyncInnDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:ProdConn"]));
+            services.AddTransient<IAmenities, AmenitiesService>();
+            services.AddTransient<IHotel, HotelService>();
+            services.AddTransient<IRoom, RoomService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
